0c22550baa48abdbc44587dfba5e81b53ce1bf00
gingerdan <dan@wearecrew.com>
2018-11-30 17:45:20 +0000

    updated site Amanda

---
+++ src/components/mainNav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const MainNav = ({ siteTitle }) => (
+  <div>Some text
+    
+  </div>
+)
+
+export default MainNav
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,54 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-const IndexPage = () => (
-  <div>
-    <div className="Hero">
-      <div className="HeroGroup">
-        <img width="500" src={require('../images/Skute_logo_cross.svg')}></img>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <Link to="/page-2/">Watch video</Link>
+class IndexPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      overlayOpen: false,
+    }
+
+    this.handleOpen = this.handleOpen.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+  }
+
+  handleOpen() {
+    this.setState({ overlayOpen: true });
+  }
+
+  handleClose() {
+    this.setState({ overlayOpen: false });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.overlayOpen && (
+          <div className="overlay">
+            <div className="close-overlay" onClick={this.handleClose} />
+          </div>
+        )}
+        <div className="Hero">
+          <div className="HeroGroup">
+            <img
+              className="skute-logo"
+              src={require('../images/Skute_logo_cross.svg')}
+            />
+            <button onClick={this.handleOpen} className="black-button">Watch video</button>
+            <h1>The worlds first physical to digital fan network</h1>
+            <div className="AppStoreButtons">
+              <a href="https://itunes.apple.com/us/app/skute/id1438624407?ls=1&mt=8">
+                <img src={require('../images/AppleStoreButton.svg')} />
+              </a>
+              <a href="/">
+                <img src={require('../images/googlePlayButton.svg')} />
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-    
-  </div>
-)
+    )
+  }
+}
 
 export default IndexPage
