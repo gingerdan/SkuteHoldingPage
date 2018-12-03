672e38fc0698e3f1c0de7c16e6752c4d443b80d0
gingerdan <dan@wearecrew.com>
2018-12-03 17:26:40 +0000

    updated with video overlay

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,16 +8,16 @@ class IndexPage extends React.Component {
       overlayOpen: false,
     }
 
-    this.handleOpen = this.handleOpen.bind(this);
-    this.handleClose = this.handleClose.bind(this);
+    this.handleOpen = this.handleOpen.bind(this)
+    this.handleClose = this.handleClose.bind(this)
   }
 
   handleOpen() {
-    this.setState({ overlayOpen: true });
+    this.setState({ overlayOpen: true })
   }
 
   handleClose() {
-    this.setState({ overlayOpen: false });
+    this.setState({ overlayOpen: false })
   }
 
   render() {
@@ -25,25 +25,35 @@ class IndexPage extends React.Component {
       <div>
         {this.state.overlayOpen && (
           <div className="overlay">
-            <div className="close-overlay" onClick={this.handleClose} />
+            <img
+              className="close-overlay"
+              src={require('../images/CloseButton.svg')}
+              onClick={this.handleClose}
+            />
+            <iframe
+              className="iframe"
+              src="https://player.vimeo.com/video/303678443"
+              frameborder="0"
+              allowfullscreen
+            />
           </div>
         )}
-        <div className="Hero">
-          <div className="HeroGroup">
-            <img
-              className="skute-logo"
-              src={require('../images/Skute_logo_cross.svg')}
-            />
-            <button onClick={this.handleOpen} className="black-button">Watch video</button>
-            <h1>The worlds first physical to digital fan network</h1>
-            <div className="AppStoreButtons">
-              <a href="https://itunes.apple.com/us/app/skute/id1438624407?ls=1&mt=8">
-                <img src={require('../images/AppleStoreButton.svg')} />
-              </a>
-              <a href="/">
-                <img src={require('../images/googlePlayButton.svg')} />
-              </a>
-            </div>
+        <div className="wrapper">
+          <img
+            className="skute-logo"
+            src={require('../images/Skute_logo_cross.svg')}
+          />
+          <button onClick={this.handleOpen} className="video-button">
+            Watch video
+          </button>
+          <h1 className="tagline">The worlds first physical to digital fan network</h1>
+          <div className="app-store-buttons">
+            <a href="https://itunes.apple.com/us/app/skute/id1438624407?ls=1&mt=8">
+              <img className="logo" src={require('../images/AppleStoreButton.svg')} />
+            </a>
+            <a href="/">
+              <img className="logo" src={require('../images/googlePlayButton.svg')} />
+            </a>
           </div>
         </div>
       </div>
